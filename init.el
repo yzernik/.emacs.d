7267f36db4f8eca7d787970869c5989146bbe2df
Jonathan Zernik <jonathan.zernik@elementum.com>
2015-01-15 15:26:39 -0800

    add comments

--- init.el
+++ init.el
@@ -1,10 +1,12 @@
-(require 'package) ;; You might already have this line
+(require 'package)
+
+;; add MELPA
 (add-to-list 'package-archives
              '("melpa-stable" . "http://stable.melpa.org/packages/") t)
 (when (< emacs-major-version 24)
   ;; For important compatibility libraries like cl-lib
   (add-to-list 'package-archives '("gnu" . "http://elpa.gnu.org/packages/")))
-(package-initialize) ;; You might already have this line
+(package-initialize)
 
 
 ;; add clojure-mode
