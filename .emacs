a61baa8c612f1085a21ce4ed470e4d7eec990da7
Jonathan Zernik <jonathan.zernik@elementum.com>
2015-01-15 14:50:06 -0800

    added clojure mode

--- .emacs
+++
@@ -1,4 +0,0 @@
-;; for flex files
-(autoload 'flex-mode "flex-mode" "flex mode" t)
-(setq auto-mode-alist (append '(("\\.l$" . flex-mode))
-auto-mode-alist))
--- init.el
+++ init.el
@@ -1,2 +1,15 @@
-;; add to tuareg-mode
-(load "~/.emacs.d/ocaml/tuareg-site-file")
+(require 'package) ;; You might already have this line
+(add-to-list 'package-archives
+             '("melpa-stable" . "http://stable.melpa.org/packages/") t)
+(when (< emacs-major-version 24)
+  ;; For important compatibility libraries like cl-lib
+  (add-to-list 'package-archives '("gnu" . "http://elpa.gnu.org/packages/")))
+(package-initialize) ;; You might already have this line
+
+
+;; add clojure-mode
+(unless (package-installed-p 'clojure-mode)
+  (package-refresh-contents))
+;; add CIDER
+(unless (package-installed-p 'cider)
+  (package-install 'cider))
