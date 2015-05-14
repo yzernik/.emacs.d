10cdcbb0c7cbe5d23454092babc17ab0fca8756a
yzernik <yzernik@gmail.com>
2015-05-13 23:47:57 -0700

    display both line and column numbers

--- init.el
+++ init.el
@@ -5,6 +5,9 @@
 (add-to-list 'default-frame-alist '(fullscreen . maximized))
 (setq inhibit-startup-message t)
 
+;; Display line and column numbers.
+(setq line-number-mode t)
+(setq column-number-mode t)
 
 ;; Use MELPA
 (add-to-list 'load-path (expand-file-name "lisp" user-emacs-directory))
