fb0678a582afe9949999339997fa457758efa0d5
yzernik <yzernik@gmail.com>
2015-08-17 18:15:40 -0700

    go back to show-paren-mode

--- lisp/init-parens.el
+++ lisp/init-parens.el
@@ -1,14 +1,8 @@
 
 
-;; parenthesis/brackets balancing and navigating
-;; https://github.com/Fuco1/smartparens
-(use-package smartparens
-  :ensure t
-  :config (progn (require 'smartparens-config)
-		 (smartparens-global-mode t)
-		 ;; highlight matching pairs
-		 (show-smartparens-global-mode t)
-		 (add-hook 'clojure-mode-hook 'smartparens-strict-mode)
-		 (add-hook 'emacs-lisp-mode-hook 'smartparens-strict-mode)))
+;; add show-paren mode
+(setq show-paren-delay 0)
+(show-paren-mode 1)
+
 
 (provide 'init-parens)
