16988d2ebd7826c3960de7f2667f8cf992d71a5e
Jonathan Zernik <jzernik@yelp.com>
2016-05-02 21:44:54 -0700

    Add anaconda-mode backend for company.

--- lisp/init-python.el
+++ lisp/init-python.el
@@ -2,6 +2,12 @@
 ;; add anaconda-mode
 (require-package 'anaconda-mode)
 
+;; add company-anaconda
+(require-package 'company-anaconda)
+(eval-after-load "company"
+  '(add-to-list 'company-backends 'company-anaconda))
+(add-hook 'python-mode-hook 'anaconda-mode)
+
 (add-hook 'python-mode-hook 'anaconda-mode)
 
 (provide 'init-python)
