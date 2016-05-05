530879132c4a3f6bf97f216a1e556418de6b235a
Jonathan Zernik <jzernik@yelp.com>
2016-05-04 22:08:39 -0700

    disable 80-column rule

--- lisp/init-column-rule.el
+++ lisp/init-column-rule.el
@@ -7,6 +7,6 @@
 
 (require 'fill-column-indicator)
 (define-globalized-minor-mode global-fci-mode fci-mode (lambda () (fci-mode 1)))
-(global-fci-mode 1)
+;(global-fci-mode 1)
 
 (provide 'init-column-rule)
