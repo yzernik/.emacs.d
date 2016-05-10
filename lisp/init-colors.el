8eb44983758cdd1ee7f58e82b3c421eeed1d1b92
Jonathan Zernik <jzernik@yelp.com>
2016-05-10 10:53:35 -0700

    switch back to cyberpunk theme

--- lisp/init-colors.el
+++ lisp/init-colors.el
@@ -1,11 +1,11 @@
 
 ;; add color themes
-(require-package 'material-theme)
+(require-package 'cyberpunk-theme)
 (require-package 'rainbow-delimiters)
 
 (add-to-list 'custom-theme-load-path "~/.emacs.d/themes")
 
-;; Use the cyberpunks color theme
-(load-theme 'material t)
+;; Use the cyberpunk color theme
+(load-theme 'cyberpunk t)
 
 (provide 'init-colors)
