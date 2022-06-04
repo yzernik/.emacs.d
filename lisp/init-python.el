4d127afff53ab673d0e13c8d7c265d711d2c003b
Jonathan Zernik <yzernik@gmail.com>
2022-06-03 20:07:26 -0700

    Update rust mode and python mode

--- lisp/init-python.el
+++ lisp/init-python.el
@@ -1,18 +1,18 @@
 
-;; add company-jedi mode
-(require-package 'company-jedi)
+;; ;; add company-jedi mode
+;; (require-package 'company-jedi)
 
-(defun my/python-mode-hook ()
-  (add-to-list 'company-backends 'company-jedi))
+;; (defun my/python-mode-hook ()
+;;   (add-to-list 'company-backends 'company-jedi))
 
-(add-hook 'python-mode-hook 'my/python-mode-hook)
+;; (add-hook 'python-mode-hook 'my/python-mode-hook)
 
-(jedi:install-server)
+;; (jedi:install-server)
 
-;; configure jedi to use Python 3
-(setq jedi:environment-root "jedi")  ; or any other name you like
-(setq jedi:environment-virtualenv
-      (append python-environment-virtualenv
-              '("--python" "/usr/bin/python3")))
+;; ;; configure jedi to use Python 3
+;; (setq jedi:environment-root "jedi")  ; or any other name you like
+;; ;; (setq jedi:environment-virtualenv
+;; ;;       (append python-environment-virtualenv
+;; ;;               '("--python" "/usr/bin/python3")))
 
 (provide 'init-python)
